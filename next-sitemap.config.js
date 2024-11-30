1af32e54ce3ada4d1abe97c27b4ef14a2f73cbad
willyam ribiere <willyamrib@gmail.com>
2024-11-30 14:42:07 +0100

    Add sitemap.xml to public directory1.1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,7 @@ module.exports = {
   siteUrl: "https://www.eclairciepaysages.fr/", // Remplacez par votre URL
   generateRobotsTxt: true, // Génère automatiquement un fichier robots.txt
   sitemapSize: 5000, // Nombre maximum d'URL dans chaque fichier sitemap
+  changefreq: "weekly", // Fréquence de mise à jour suggérée pour les pages
+  priority: 0.7, // Priorité par défaut pour les pages
+  exclude: ["/404"],
 };
