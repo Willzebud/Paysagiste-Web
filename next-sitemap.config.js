f0359677bf20684bbf11a64fbdf3f31fed1aa783
willyam ribiere <willyamrib@gmail.com>
2024-11-30 15:07:48 +0100

    Fix sitemap generation1.2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.eclairciepaysages.fr/", // Remplacez par votre URL
-  generateRobotsTxt: true, // Génère automatiquement un fichier robots.txt
-  sitemapSize: 5000, // Nombre maximum d'URL dans chaque fichier sitemap
-  changefreq: "weekly", // Fréquence de mise à jour suggérée pour les pages
-  priority: 0.7, // Priorité par défaut pour les pages
+  siteUrl: "https://www.eclairciepaysages.fr/",
+  generateRobotsTxt: true,
+  sitemapSize: 5000, // Assurez-vous que la taille est suffisante pour inclure toutes les URLs
+  generateIndexSitemap: false, // Désactive la génération d'un index de sous-sitemaps
   exclude: ["/404"],
-  generateIndexSitemap: true,
 };
