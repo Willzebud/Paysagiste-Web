5354cb0eeaa5f2d694c3e2bda36c5ffb15654cc8
willyam ribiere <willyamrib@gmail.com>
2024-12-04 11:25:32 +0100

    Correction1.5

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,4 +5,11 @@ module.exports = {
   sitemapSize: 5000, // Assurez-vous que la taille est suffisante pour inclure toutes les URLs
   generateIndexSitemap: false, // Désactive la génération d'un index de sous-sitemaps
   exclude: ["/404"],
+  additionalPaths: async () => [
+    {
+      loc: "/logo.webp", // Inclure le logo
+      changefreq: "yearly",
+      priority: 1.0,
+    },
+  ],
 };
